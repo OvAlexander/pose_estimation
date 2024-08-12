696c4d36df94d9f8035a845eb2be9fbc3e71cd66
OvAlexander <alexanderleeov@gmail.com>
2024-08-12 13:12:28 -0700

    feat: got mediapipes to work and can track basic movement

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
 import cv2 as cv
 import numpy as np
 
+PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
+
+def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('pose landmarker result: {}'.format(result))
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+  pose_landmarks_list = detection_result.pose_landmarks
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected poses to visualize.
+  for idx in range(len(pose_landmarks_list)):
+    pose_landmarks = pose_landmarks_list[idx]
+
+    # Draw the pose landmarks.
+    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    pose_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      pose_landmarks_proto,
+      solutions.pose.POSE_CONNECTIONS,
+      solutions.drawing_styles.get_default_pose_landmarks_style())
+  return annotated_image
+
 model_path = r'C:\\Users\\alexo\Documents\\projects\\pose_estimation\\pose_landmarker_lite.task'
 
-BaseOption = mp.tasks.BaseOptions
-PoseLandmarker = mp.tasks.vision.PoseLandmarker
-PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
-VisionRunningMode = mp.tasks.vision.RunningMode
+# BaseOption = mp.tasks.BaseOptions
+# PoseLandmarker = mp.tasks.vision.PoseLandmarker
+# PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+# VisionRunningMode = mp.tasks.vision.RunningMode
+BaseOptions = python.BaseOptions(model_asset_path=model_path)
+options = vision.PoseLandmarkerOptions(base_options=BaseOptions,running_mode=vision.RunningMode.VIDEO, output_segmentation_masks=True)
+detector = vision.PoseLandmarker.create_from_options(options)
+# options = PoseLandmarkerOptions(base_options = BaseOption(model_asset_path=model_path), running_mode=VisionRunningMode.VIDEO)
 
-options = PoseLandmarkerOptions(base_options = BaseOption(model_asset_path=model_path), running_mode=VisionRunningMode.VIDEO)
+# detector = PoseLandmarker.create_from_options(options)
 
-dector = PoseLandmarker.create_from_options(options)
 
 video = cv.VideoCapture(0)
 
@@ -25,7 +55,9 @@ while(True):
     np_frame = np.stack(frame, axis = 0)
     frame_timestamp_ms = video.get(cv.CAP_PROP_POS_MSEC)
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np_frame)
-    PoseLandmarker.detect_for_video(mp_image, int(frame_timestamp_ms))
+    result = detector.detect_for_video(mp_image, int(frame_timestamp_ms))
+    annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), result)
+    cv.imshow('frame 2',cv.cvtColor(annotated_image, cv.COLOR_RGB2BGR))
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
