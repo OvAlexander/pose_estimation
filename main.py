aa3a0323dafd3f35f97ea15a42b3596b485f807f
OvAlexander <alexanderleeov@gmail.com>
2024-08-12 13:17:28 -0700

    chore: cleaned up code by moving things around to make it more readable

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from mediapipe.framework.formats import landmark_pb2
 import cv2 as cv
 import numpy as np
 
+model_path = r'C:\\Users\\alexo\Documents\\projects\\pose_estimation\\pose_landmarker_lite.task'
+BaseOptions = python.BaseOptions(model_asset_path=model_path)
+options = vision.PoseLandmarkerOptions(base_options=BaseOptions,running_mode=vision.RunningMode.VIDEO, output_segmentation_masks=True)
+detector = vision.PoseLandmarker.create_from_options(options)
 PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
 
 def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
@@ -31,35 +35,25 @@ def draw_landmarks_on_image(rgb_image, detection_result):
       solutions.drawing_styles.get_default_pose_landmarks_style())
   return annotated_image
 
-model_path = r'C:\\Users\\alexo\Documents\\projects\\pose_estimation\\pose_landmarker_lite.task'
-
-# BaseOption = mp.tasks.BaseOptions
-# PoseLandmarker = mp.tasks.vision.PoseLandmarker
-# PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
-# VisionRunningMode = mp.tasks.vision.RunningMode
-BaseOptions = python.BaseOptions(model_asset_path=model_path)
-options = vision.PoseLandmarkerOptions(base_options=BaseOptions,running_mode=vision.RunningMode.VIDEO, output_segmentation_masks=True)
-detector = vision.PoseLandmarker.create_from_options(options)
-# options = PoseLandmarkerOptions(base_options = BaseOption(model_asset_path=model_path), running_mode=VisionRunningMode.VIDEO)
-
-# detector = PoseLandmarker.create_from_options(options)
 
 
-video = cv.VideoCapture(0)
 
-frame_timestamp_ms = video.get(cv.CAP_PROP_POS_MSEC)
-calc_timestamps = [0.0]
-while(True):
-    ret, frame = video.read()
-    cv.imshow('frame',frame)
-    np_frame = np.stack(frame, axis = 0)
+if __name__ == '__main__':
+    video = cv.VideoCapture(0)
+
     frame_timestamp_ms = video.get(cv.CAP_PROP_POS_MSEC)
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np_frame)
-    result = detector.detect_for_video(mp_image, int(frame_timestamp_ms))
-    annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), result)
-    cv.imshow('frame 2',cv.cvtColor(annotated_image, cv.COLOR_RGB2BGR))
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+    calc_timestamps = [0.0]
+    while(True):
+        ret, frame = video.read()
+        cv.imshow('frame',frame)
+        np_frame = np.stack(frame, axis = 0)
+        frame_timestamp_ms = video.get(cv.CAP_PROP_POS_MSEC)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np_frame)
+        result = detector.detect_for_video(mp_image, int(frame_timestamp_ms))
+        annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), result)
+        cv.imshow('frame 2',cv.cvtColor(annotated_image, cv.COLOR_RGB2BGR))
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
 
-video.release()
-cv.destroyAllWindows
+    video.release()
+    cv.destroyAllWindows
